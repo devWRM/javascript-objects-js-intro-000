f2daba49d37fa8d7ce650df35fddf1d9fdcab20c
Wendy Raven McNair <wen.mcnair@gmail.com>
2020-02-14 21:49:13 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,6 +4,6 @@ var playlist = {
 
 function updatePlaylist(playlist, artistName, songTitle) {
 
-  playlist.artistName = ["songTitle"];
+  return Object.assign(playlist, {[artistName]: songTitle});
 
 }
